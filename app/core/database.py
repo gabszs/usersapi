cfce1f5f8b124047f2e184bd966fb7368ca8fa51
Gabriel <115580274+gabszs@users.noreply.github.com>
2024-01-05 12:18:16 -0300

    refactor(DatabaseUrl): Refactoring the databse url to inside get_session function to allow dynamic tests

--- app/core/database.py
+++ app/core/database.py
@@ -3,10 +3,9 @@ from sqlalchemy.orm import Session
 
 from app.core.settings import Settings
 
-TEST_MODE = Settings().TEST_MODE
-engine = create_engine(Settings().DATABASE_URL if not TEST_MODE else Settings().TEST_DATABASE_URL)
-
 
 def get_session():
+    database_url = Settings().DATABASE_URL if not Settings().TEST_MODE else Settings().TEST_DATABASE_URL
+    engine = create_engine(database_url)
     with Session(engine) as session:
         yield session
